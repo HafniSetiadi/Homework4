90bc655bc162699d063f4cf85c1e0cb84e4ace11
HafniSetiadi <106217072+HafniSetiadi@users.noreply.github.com>
2022-07-17 02:58:47 +0700

    Coba buat koneksi - binding

---
+++ app/src/main/java/com/setiadi/homework4/SampleActivity1.kt
@@ -0,0 +1,11 @@
+package com.setiadi.homework4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SampleActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample1)
+    }
+}
