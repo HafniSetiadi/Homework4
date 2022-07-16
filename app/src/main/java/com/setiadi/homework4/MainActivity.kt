28ee8d76e71962777b3fa16e225718ccee9730b2
HafniSetiadi <106217072+HafniSetiadi@users.noreply.github.com>
2022-07-17 02:56:01 +0700

    Coba buat koneksi - binding

--- app/src/main/java/com/setiadi/homework4/MainActivity.kt
+++ app/src/main/java/com/setiadi/homework4/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.setiadi.homework4
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import com.setiadi.homework4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnSubmit?.setOnClickListener {
+            binding?.tvHelloWorld?.text ="Yeay akhirnya bisa terkoneksi!!"
+        }
     }
 }
